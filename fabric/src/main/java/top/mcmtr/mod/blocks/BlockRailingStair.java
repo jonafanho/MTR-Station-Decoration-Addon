1597070be0eb4791a40a074059345ade8dab0e9d
AIDA64S <zxc17634985169@gmail.com>
2024-07-04 00:42:01 +0800

    add new models

--- fabric/src/main/java/top/mcmtr/mod/blocks/BlockRailingStair.java
+++ fabric/src/main/java/top/mcmtr/mod/blocks/BlockRailingStair.java
@@ -14,7 +14,24 @@ public final class BlockRailingStair extends BlockChangeModelBase {
 
     @Override
     public VoxelShape getOutlineShape2(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return state.get(new Property<>(TYPE.data)) == 4 ?
-                (this.isMirror ? IBlock.getVoxelShapeByDirection(13, 0, 0, 16, 24, 3, IBlock.getStatePropertySafe(state, FACING)) : IBlock.getVoxelShapeByDirection(0, 0, 0, 3, 24, 3, IBlock.getStatePropertySafe(state, FACING))) : IBlock.getVoxelShapeByDirection(0, 0, 1, 16, 24, 3, IBlock.getStatePropertySafe(state, FACING));
+        final int index = state.get(new Property<>(TYPE.data));
+        if(index == 4){
+            return this.isMirror ?
+                    IBlock.getVoxelShapeByDirection(13, 0, 0, 16, 24, 3, IBlock.getStatePropertySafe(state, FACING)) :
+                    IBlock.getVoxelShapeByDirection(0, 0, 0, 3, 24, 3, IBlock.getStatePropertySafe(state, FACING));
+        }
+        if(index == 5) {
+            VoxelShape voxelShape1;
+            VoxelShape voxelShape2;
+            if(this.isMirror) {
+                voxelShape1 = IBlock.getVoxelShapeByDirection(1, 0, 1, 16, 24, 3, IBlock.getStatePropertySafe(state, FACING));
+                voxelShape2 = IBlock.getVoxelShapeByDirection(1, 0, 1, 3, 24, 16, IBlock.getStatePropertySafe(state, FACING));
+            } else {
+                voxelShape1 = IBlock.getVoxelShapeByDirection(0, 0, 1, 15, 24, 3, IBlock.getStatePropertySafe(state, FACING));
+                voxelShape2 = IBlock.getVoxelShapeByDirection(13, 0, 1, 15, 24, 16, IBlock.getStatePropertySafe(state, FACING));
+            }
+            return VoxelShapes.union(voxelShape1,voxelShape2);
+        }
+        return IBlock.getVoxelShapeByDirection(0, 0, 1, 16, 24, 3, IBlock.getStatePropertySafe(state, FACING));
     }
 }
